53124af128590262b52fea4eb937bb2d324de1c6
Odinbytes <jabwunza@gmail.com>
2025-06-17 12:50:36 +0300

    Building the navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
     return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* Theme Toggle */}
         <ThemeToggle />
+
         {/* Background Effects */}
         <StarBackground />
+
+        {/* Navigation Bar */}
+        <Navbar />
+
     </div>
     );
 };
